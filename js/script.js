a485491d07c17f76044f4ff98a75cf03d257990d
pupsione <delait.tryz000@gmail.com>
2021-06-05 12:32:51 +0200

    finish

---
+++ js/script.js
@@ -0,0 +1,121 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => { /* скрипты загружаються после сформирование DOM*/
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        promo = document.querySelector('.promo__bg'),
+        genre = promo.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault(); /* отменна стандартоного поведения кнопки*/
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) { /* условия при котором не добавляется пустая строка*/
+
+            if (newFilm.length > 21) {/* условия при котором название которое больше чем 21 обрезается*/
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            
+            if (favorite){
+                console.log("Добавляем любимый фильм");
+            }
+            movieDB.movies.push(newFilm); /* пушим новый фильм в массив*/
+            sortArr(movieDB.movies);
+        }
+
+        createMovieList(movieDB.movies, movieList);
+
+        e.target.reset(); /* очистка формы*/
+    });
+
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+
+    const makeChange = () => {
+        genre.textContent = 'драма';
+
+        promo.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+
+    const sortArr = (arr) => { /* сортировка массива по алфавиту*/
+        arr.sort();
+    };
+
+    function createMovieList(film, parent) {
+        movieList.innerHTML = ""; // Заменяет содержимое пустой строкой.
+        sortArr(film); // сортировка по алфавиту
+
+        film.forEach((film, i) => {
+            parent.innerHTML += // innerHTML = innerHTML + то что нужно присвоить или заменить
+                `<li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>`;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove(); //удаляет родительский елемент
+                movieDB.movies.splice(i, 1); // удаляет елемент с бз
+                createMovieList(movieDB.movies, movieList); //перестраивает лист, чтоб была норм нумерация
+            });
+        });
+    }
+
+
+    deleteAdv(adv);
+    makeChange();
+    createMovieList(movieDB.movies, movieList);
+});
